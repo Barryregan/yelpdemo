1d8d3c9267764b548882f8bf35e622743df334af
Barry Regan <barryregan101@gmail.com>
2018-03-05 17:56:49 +0000

    change region for aws bucket and new access key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAI3EV37YNS4L7X7XQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     'AKIAJTGYZ77J5MONO6NQ',                        # required
+    aws_secret_access_key: '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',                        # required
   }
-  config.fog_directory  = 'barryelp2'                     # required
+  config.fog_directory  = 'barryelp'                     # required
 end
