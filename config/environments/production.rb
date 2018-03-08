3667e324460bd9077f8ae4bb43f5fff15c33b2d4
Barry Regan <barryregan101@gmail.com>
2018-03-08 16:05:09 +0000

    oh, you know

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   #required for devise - remember to change to actual application host
-  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
 end
